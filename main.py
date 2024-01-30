6e8bcca8c621491ba433e644b62d9b8a628bfd1e
Дмитрий <SherDS@mail.ru>
2024-01-30 20:38:29 +0300

    Добавили добавили цикл for

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def division(a: int|float, b: int|float):
 
 def main():
     for i, j in enumerate([1, 2, 3], 1):
-    d = division(i, j)
+        d = division(i, j)
 
-    print(d)
+        print(d)
 
 
 
